df4e90d73842f57ae049ecfa78ca8b3fa53728ab
MathiasMendoza <mathiasmendoza_vargas14@hotmail.com>
2023-07-05 23:04:35 -0700

    New pages structures

---
+++ public/js/previewImage.js
@@ -0,0 +1,18 @@
+const chooseFile = document.getElementById('image-input');
+const imgPreview = document.getElementById("img-preview");
+
+function getImgData() {
+    const files = chooseFile.files[0];
+    if (files) {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(files);
+        fileReader.addEventListener("load", function () {
+            imgPreview.style.display = "block";
+            imgPreview.innerHTML = '<img src="' + this.result + '" />';
+        });    
+    }
+}
+
+chooseFile.addEventListener("change", function () {
+    getImgData();
+});
